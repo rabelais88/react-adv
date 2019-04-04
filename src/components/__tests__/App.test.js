40dbe1e3db945651fc173ff9fff52f9702c08d42
Sungryeol Park <sungryeolp@gmail.com>
2019-04-05 01:45:28 +0900

    absolute path

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import App from '../App';
+import App from 'components/App';
 import { shallow } from 'enzyme';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const wrapped = shallow(<App />);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from 'components/App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+import reducers from 'reducers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
